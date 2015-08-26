be652d43cd164276c4fe623b28c32d9a2618c84b
Alan <alanly211@gmail.com>
2015-08-25 18:13:50 -0700

    set onloadstart to make loop btn

--- src/loop.js
+++ src/loop.js
@@ -4,20 +4,25 @@ function apply_loop_to_video(loop_status) {
   video.attr("loop",loop_status);
 };
 
-var video_menu = $(".ytp-menu#ytp-main-menu-id");
-var loop_menu_item = $("<div id=\"ytp-menuitem-loop\" class=\"ytp-menuitem\" role=\"menuitemcheckbox\" tabindex=\"38\" aria-checked=\"false\"> \
-                      <div class=\"ytp-menuitem-label\">Loop</div> \
-                      <div class=\"ytp-menuitem-content\"> \
-                        <div id=\"ytp-loop-checkbox\" class=\"ytp-menuitem-toggle-checkbox\"> \
+function setup_loop_btn() {
+  var video_menu = $(".ytp-menu#ytp-main-menu-id");
+  var loop_menu_item = $("<div id=\"ytp-menuitem-loop\" class=\"ytp-menuitem\" role=\"menuitemcheckbox\" tabindex=\"38\" aria-checked=\"false\"> \
+                        <div class=\"ytp-menuitem-label\">Loop</div> \
+                        <div class=\"ytp-menuitem-content\"> \
+                          <div id=\"ytp-loop-checkbox\" class=\"ytp-menuitem-toggle-checkbox\"> \
+                          </div> \
                         </div> \
-                      </div> \
-                    </div>");
+                      </div>");
 
-video_menu.append(loop_menu_item);
-var loop_check_box = $("#ytp-loop-checkbox").get(0);
-loop_check_box.onclick = function() {
-  var checkbox_status_attr = "aria-checked";
-  var new_attr = !(loop_menu_item.attr(checkbox_status_attr) === 'true');
-  loop_menu_item.attr(checkbox_status_attr, new_attr);
-  apply_loop_to_video(new_attr);
+  video_menu.append(loop_menu_item);
+  var loop_check_box = $("#ytp-loop-checkbox").get(0);
+  loop_check_box.onclick = function() {
+    var checkbox_status_attr = "aria-checked";
+    var new_attr = !(loop_menu_item.attr(checkbox_status_attr) === 'true');
+    loop_menu_item.attr(checkbox_status_attr, new_attr);
+    apply_loop_to_video(new_attr);
+  };
+  debugger;
 };
+
+video.get(0).onloadstart = setup_loop_btn;
