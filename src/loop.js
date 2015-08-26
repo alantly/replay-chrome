4aa6e7af743bc7191c8885f19549a7c2051782fd
Alan <alanly211@gmail.com>
2015-08-26 08:20:43 -0700

    refactored code and made settings btn as the hook instead of a race on loading video vs js

--- src/loop.js
+++ src/loop.js
@@ -1,28 +1,52 @@
 var video = $(".video-stream.html5-main-video");
+var loop_btn_id = "ytp-loop-checkbox";
+var ytp_menu_popup_id = ".ytp-menu#ytp-main-menu-id"
+var ytp_menu_btn_id = ".ytp-button.ytp-settings-button"
+
 
 function apply_loop_to_video(loop_status) {
   video.attr("loop",loop_status);
 };
 
-function setup_loop_btn() {
-  var video_menu = $(".ytp-menu#ytp-main-menu-id");
-  var loop_menu_item = $("<div id=\"ytp-menuitem-loop\" class=\"ytp-menuitem\" role=\"menuitemcheckbox\" tabindex=\"38\" aria-checked=\"false\"> \
-                        <div class=\"ytp-menuitem-label\">Loop</div> \
-                        <div class=\"ytp-menuitem-content\"> \
-                          <div id=\"ytp-loop-checkbox\" class=\"ytp-menuitem-toggle-checkbox\"> \
-                          </div> \
-                        </div> \
-                      </div>");
+function add_loop_btn_to_menu() {
+  var video_menu = $(ytp_menu_popup_id);
+  video_menu.prepend(create_loop_btn(loop_btn_id));
+};
 
-  video_menu.append(loop_menu_item);
-  var loop_check_box = $("#ytp-loop-checkbox").get(0);
+function create_loop_btn(new_btn_id) {
+  var loop_menu_item = $("<div id=\"ytp-menuitem-loop\" class=\"ytp-menuitem\" role=\"menuitemcheckbox\" tabindex=\"38\" aria-checked=\"false\"> \
+                            <div class=\"ytp-menuitem-label\">Loop</div> \
+                            <div class=\"ytp-menuitem-content\"> \
+                              <div id=\"" + new_btn_id + "\" class=\"ytp-menuitem-toggle-checkbox\"> \
+                              </div> \
+                            </div> \
+                          </div>");
+
+  var loop_check_box = loop_menu_item.find(add_hash(new_btn_id)).get(0);
+
   loop_check_box.onclick = function() {
     var checkbox_status_attr = "aria-checked";
     var new_attr = !(loop_menu_item.attr(checkbox_status_attr) === 'true');
     loop_menu_item.attr(checkbox_status_attr, new_attr);
     apply_loop_to_video(new_attr);
   };
-  debugger;
+
+  return loop_menu_item;
 };
 
-video.get(0).onloadstart = setup_loop_btn;
+function item_exists(item_tag) {
+  return $(add_hash(item_tag)).length > 0;
+};
+
+function add_hash(tag) {
+  return "#"+tag;
+};
+
+// video.get(0).onloadstart = add_loop_btn_to_menu;
+var setting_btn = $(ytp_menu_btn_id).get(0);
+setting_btn.onmouseenter = function() {
+  if (!item_exists(loop_btn_id)) {
+    add_loop_btn_to_menu();
+  }
+};
+
